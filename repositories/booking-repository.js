754d8796f44b53ae0cc537b19b54ab20bbb231b5
akil <akileshjm@gmail.com>
2026-03-05 16:18:39 +0530

    Added cancel booking logic with cron

--- repositories/booking-repository.js
+++ repositories/booking-repository.js
@@ -1,7 +1,9 @@
 const {StatusCodes} = require("http-status-codes");
-
+const {Op} = require('sequelize');
 const {Booking} = require('../models');
 const CrudRepository = require('./crud-repository');
+const {Enums} = require('../utils/common');
+const {BOOKED,CANCELLED} = Enums.BOOKING_STATUS;
 
 class BookingRepository extends CrudRepository{
     constructor(){
@@ -15,7 +17,7 @@ class BookingRepository extends CrudRepository{
     }
 
     async get(data, transaction){
-        const response = await this.model.findByPk(data , {transaction: transaction});
+        const response = await Booking.findByPk(data , {transaction: transaction});
         if(!response){
             throw new AppError ('Not able to find the resource' , StatusCodes.NOT_FOUND);
         }
@@ -24,13 +26,51 @@ class BookingRepository extends CrudRepository{
 
     async update(id, data , transaction){
         // data -> {col: value,...}
-        const response = await this.model.update(data, {
+        const response = await Booking.update(data, {
             where:{
                 id:id,
             }
         },{transaction: transaction});
         return response;
     }
+
+
+    async cancelOldBookings(timestamp){
+      const response = await Booking.update({status: CANCELLED},{
+        where:{
+            [Op.and]:  [
+                {
+                     createdAt: {
+                [Op.lt]:timestamp
+            }
+        },
+
+            {
+                status: {
+                    [Op.ne]: BOOKED
+                }
+            },
+
+            {
+                status: {
+                    [Op.ne]: CANCELLED
+                }
+            }
+
+
+
+
+
+        ]
+            
+        }
+      }
+    );
+    return response;
+    }
+
+
+
 
 }
 
